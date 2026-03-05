85ef045e7b16cfb4c98e2c272167cc7368b42395
SLIME <purpleblueslime@gmail.com>
2026-03-05 18:20:40 +0530

    fix: `api/clean.js`

--- app/api/clean/route.js
+++ app/api/clean/route.js
@@ -2,22 +2,34 @@ import { NextResponse } from 'next/server';
 import mongo from '../../../back/mongo.js';
 import supabase from '../../../back/supabase.js';
 
-export async function GET() {
-  const now = new Date();
-  const ex = new Date(now.getTime() - 86400000); // 24 hrs <=
+export const dynamic = 'force-dynamic';
 
-  const pokes = await mongo.collection('pokes').find({ createdAt: { $lt: ex } }).toArray();
+export async function GET() {
+
+  const now = new Date();
+  const ex = new Date(now.getTime() - 86400000);
+
+  const query = { createdAt: { $lt: ex } };
+
+  const pokes = await mongo.collection('pokes').find(query).toArray();
 
   const files = pokes.map(
-    (pke) => `pokes/${pke.id}.${pke.is == 'img' ? 'jpg' : 'mp4'}`
+    (pke) => `pokes/${pke.id}.${pke.is === 'img' ? 'jpg' : 'mp4'}`
   );
+
   const { error } = await supabase.storage.from('poke').remove(files);
 
   if (error) {
-    return NextResponse.json({ note: 'try again' }, { status: 500 });
+    return NextResponse.json(
+      { note: 'try again' },
+      { status: 500 }
+    );
   }
 
   await mongo.collection('pokes').deleteMany(query);
 
-  return NextResponse.json({ note: 'clean completed', cleanedAt: Date.now() });
+  return NextResponse.json({
+    note: 'clean completed',
+    cleanedAt: Date.now()
+  });
 }
